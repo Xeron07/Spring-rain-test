70deb85f7a3a63d560240ec70eac9848d63c7a57
Nihal <md.minhazuddin.nihal@gmail.com>
2020-08-26 14:34:03 +0600

    path error

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ mongoose.connect(process.env.DB_CONNECTION, {
 app.use("/api", api);
 
 //CONFIGURATION FOR PRODUCTION
-if (process.env.NODE_ENV === "production") {
+
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+
 
 //SERVER CREATION
 const PORT = process.env.PORT || 5000;
