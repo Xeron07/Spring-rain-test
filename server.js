0e3e1a08b2955c5fc24714754d408ca395d09ad3
Nihal <md.minhazuddin.nihal@gmail.com>
2020-08-26 14:22:46 +0600

    path error solved

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const path = require("path");
 const mongoose = require("mongoose");
 const api = require("./controller/contact");
 
