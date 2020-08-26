c40d95d151b222cf7c3046c6a748356501435b22
Nihal <md.minhazuddin.nihal@gmail.com>
2020-08-26 08:38:19 +0600

    controller added

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const express = require("express");
 const expressSession = require("express-session");
+const api = require("./controller/contact");
 
 const app = express();
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api", api);
 
 app.listen(5000, (req, res) => {
   console.log("Started");
